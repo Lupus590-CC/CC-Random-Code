651889bf16498d40d28f188c9eab9e93db3d4393
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2016-10-03 09:28:15 +0100

    api which will accept vanilla lua load methods

---
+++ src (kinda)/dofile and loadAPI compatable API's.lua
@@ -0,0 +1,7 @@
+-- source: http://www.computercraft.info/forums2/index.php?/topic/27663-dofile-in-mc1710cc174/page__view__findpost__p__260162
+
+local stitch = shell and {} or getfenv() -- either set things in a table (for dofile) or the environment (for os.loadAPI)
+
+--define the api here
+
+return stitch -- if it's dofile this is necessary, if it's loadAPI this does nothing.
