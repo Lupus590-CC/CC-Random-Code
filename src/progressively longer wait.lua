3dcea54bc252045922fbc489cd0cba7bb88dc8af
Lupus590 <lupussolitarius590@gmail.com>
2020-05-25 20:48:06 +0100

    Update progressively longer wait.lua

--- src/progressively longer wait.lua
+++ src/progressively longer wait.lua
@@ -1,9 +1,14 @@
-  local sleepTime = 1
+local sleepTime = 0
   turtle.select(16)
   while turtle.getItemCount(16) > 0 do
     if not dropFunc() then
-      print("Output full, waiting "..tostring(sleepTime).." seconds.")
-      os.sleep(sleepTime)
+      local _, y = term.getCursorPos()
+      term.setCursorPos(1, y)
       sleepTime = math.min(sleepTime + math.floor(sleepTime/2) +1, maxWaitTime)
+      write("Output full, waiting "..tostring(sleepTime).." seconds.")
+      os.sleep(sleepTime)
     end
+  end
+  if sleepTime > 0 then
+    print("Continuing")
   end
