873d85ba5dbfdaecee852e4f6ffa531b05f72aef
Lupus590 <lupussolitarius590@gmail.com>
2020-06-27 22:43:58 +0100

    Update rednetListener.lua

--- src/rednetListener.lua
+++ src/rednetListener.lua
@@ -1,7 +1,11 @@
-peripheral.find("modem", function(side)
-    rednet.open(side)
-end)
+local modem = peripheral.find("modem")
+modem.open(17)
+modem.open(18)
+--modem.open(19)
+modem.open(rednet.CHANNEL_BROADCAST)
+--modem.open(rednet.CHANNEL_REPEAT)
 
 while true do
-    print(rednet.receive())
+    local _,_, from, to, message = os.pullEvent("modem_message")
+    print(from, to, message)
 end
