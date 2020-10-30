0fce7814247f555348d40984c870775a0365757a
Lupus590 <lupussolitarius590@gmail.com>
2020-10-30 23:20:52 +0000

    Update tableMerge.lua

--- src/tableMerge.lua
+++ src/tableMerge.lua
@@ -1,10 +1,22 @@
-local function tableMerge(...)
+local function shallowTableMerge(...)
     -- TODO: arg validaton
     local args = table.pack(...)
     local merged = {}
     for _, arg in ipairs(args) do
       for k, v in pairs(arg) do
         merged[k] = v
+      end
+    end
+    return merged
+  end
+
+local function deepTableMerge(...)
+    -- TODO: arg validaton
+    local args = table.pack(...)
+    local merged = {}
+    for _, arg in ipairs(args) do
+      for k, v in pairs(arg) do
+        merged[k] = v -- TODO: if v is a table then clone the table 
       end
     end
     return merged
