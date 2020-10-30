9333f973d2d500b52f8c768f61fee2a7dea3eae8
Lupus590 <lupussolitarius590@gmail.com>
2020-10-30 23:02:04 +0000

    replace ew randome number gen with nice one
    see: https://github.com/Lupus590-CC/CC-Random-Code/blob/master/src/psudoRandomId.lua

--- src/minimum delay yield.lua
+++ src/minimum delay yield.lua
@@ -1,5 +1,5 @@
 local function snooze()
-  local myEvent = tostring({}) -- make sure unique event
+  local myEvent = string.format("%08x", math.random(1, 2147483647)) -- make sure unique event
   os.queueEvent(myEvent)
   os.pullEvent(myEvent)
 end
