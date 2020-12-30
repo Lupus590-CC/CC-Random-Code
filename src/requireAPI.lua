0c1cfa119e1d238571e56be434fd065f1b594cc3
Lupus590 <lupussolitarius590@gmail.com>
2020-12-30 01:01:00 +0000

    Create requireAPI.lua

---
+++ src/requireAPI.lua
@@ -0,0 +1,41 @@
+-- loads an API file and returns it like require does
+-- compatability is NOT garanteed
+
+local expect = require("cc.expect").expect
+
+local tAPIsLoading = {}
+function requireAPI(_sPath)
+    expect(1, _sPath, "string")
+    local sName = fs.getName(_sPath)
+    if sName:sub(-4) == ".lua" then
+        sName = sName:sub(1, -5)
+    end
+    if tAPIsLoading[sName] == true then
+        printError("API " .. sName .. " is already being loaded")
+        return
+    end
+    tAPIsLoading[sName] = true
+
+    local tEnv = {}
+    setmetatable(tEnv, { __index = _G })
+    local fnAPI, err = loadfile(_sPath, nil, tEnv)
+    if fnAPI then
+        local ok, err = pcall(fnAPI)
+        if not ok then
+            tAPIsLoading[sName] = nil
+            return error("Failed to load API " .. sName .. " due to " .. err, 1)
+        end
+    else
+        tAPIsLoading[sName] = nil
+        return error("Failed to load API " .. sName .. " due to " .. err, 1)
+    end
+
+    local tAPI = {}
+    for k, v in pairs(tEnv) do
+        if k ~= "_ENV" then
+            tAPI[k] =  v
+        end
+    end
+    
+    return tAPI
+end
