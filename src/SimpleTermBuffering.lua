e79c1f6c04c8a544b5e2529a0b62a48bd5910e98
Lupus590 <lupussolitarius590@gmail.com>
2021-06-01 19:09:53 +0100

    Update SimpleTermBuffering.lua

--- src/SimpleTermBuffering.lua
+++ src/SimpleTermBuffering.lua
@@ -2,7 +2,10 @@ local x, y = term.getSize()
 local win = window.create(term.current(), 1, 1, x, y)
 term.redirect(win) -- TODO: capture old term and redirect back on program exit
 
+-- you may want/need to do an initial draw to the terminal, you can do that here.
+-- don't forget to toggle the win.setVisible for the best result
 while true do
+  local event = table.pack(os.pullEvent())
   win.setVisible(false)
   -- TODO: draw to term as normal
   
