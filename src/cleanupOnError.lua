b84c177271adc83c850d95563bf03c5045ccc808
Lupus590 <lupussolitarius590@gmail.com>
2021-08-31 23:41:22 +0100

    Update cleanupOnError.lua

--- src/cleanupOnError.lua
+++ src/cleanupOnError.lua
@@ -13,7 +13,7 @@ local ok, err = pcall(main) -- we catch the error so that we can clean up the co
 term.redirect(oldTerm) -- this is cleanup that we always need to do, error or not
 
 if not ok then
-    -- we could so special cleanup here that we only need to do on errors
+    -- we could do special cleanup here that we only need to do on errors
 
     error(err, 0) -- rethrow the error without adding info about this throw
                   -- it still shows in stack traces but no longer as the root error
